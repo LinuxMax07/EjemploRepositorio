bae186811a9a7887d4f2773de321fd1ab5c6803d
Blas <joseenriqueblas0307@gmail.com>
2021-01-14 19:56:56 -0600

    Segundoenvio

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Numero de veces que apreto el boton',
+              'Numero de veces en apretar el botons',
             ),
             Text(
               '$_counter',
